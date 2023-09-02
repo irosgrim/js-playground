31d9b785c76c09f3a82618a93fa5ec514c379783
Ioan Rosgrim <ioan.rosgrim@ingka.ikea.com>
2023-09-02 20:46:39 +0200

    dark,light theme

--- editor.js
+++ editor.js
@@ -1,6 +1,5 @@
 // if you want to pass code to the iframe: <iframe src="the-editor.com?code=btoa-encoded-string" 
 document.addEventListener("DOMContentLoaded", () => {
-
   const consoleOutput = document.querySelector("#console code");
   const urlParams = new URLSearchParams(window.location.search);
   const encodedCode = urlParams.get("code");
@@ -13,10 +12,12 @@ console.log("Hello world!");
   switch(theme) {
     case "dark":
       initialTheme = "nord";
+      document.body.classList.add("dark");
       break;
     default:
     case "light":
       initialTheme = "mdn-like";
+      document.body.classList.add("light");
       break;
   }
 
